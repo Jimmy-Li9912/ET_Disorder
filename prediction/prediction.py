9be5cce38f85ceb8274849acb55c45a2365fb667
LI Jingchen <61415911+Jimmy-Li9912@users.noreply.github.com>
2023-06-26 20:46:45 +0800

    Add files via upload

---
+++ prediction/prediction.py
@@ -0,0 +1,43 @@
+import torch
+import torch.nn as nn
+import numpy as np
+
+disp_dict = {0:'Disordered', 1: 'Ordered'}
+
+def disordered_predicting(model, data):
+    esm = data['esm'].cuda()
+    conf = data['conf'].cuda()
+
+    out = model(esm, conf)
+    out = out[0].detach().cpu().numpy()
+    out = np.argmax(out,axis=-1).reshape(-1)
+    #print(out.shape)
+    return out.tolist()
+def batch_prediction(model, data):
+    return
+if __name__ == '__main__':
+    model_path='./model/ckpt_cnn_2_0.719.pth'
+    parser = argparse.ArgumentParser(description='DeepCRF Baseline Model')
+    parser.add_argument('--esm', default="./demo/0_esm.pt", type=str, help='esm file')
+    parser.add_argument('--plddt', default='./demo/0_plddt.pt', type=str, help='plddt file')
+    parser.add_argument('--o', default="./demo/0.pt", type=str, help='output file')
+    args = parser.parse_args()
+
+    ESM_path = args.esm
+    PLDDT_path = args.plddt
+    out_path = args.o
+    esm = torch.load(ESM_path)[0].unsqueeze(0)
+    conf = torch.load(PLDDT_path)[0].unsqueeze(0).unsqueeze(dim=-1)
+    #print(esm.shape)
+    #print(conf.shape)
+
+    model=torch.load(model_path).cuda()
+    out = disordered_predicting(model, {'esm':esm, 'conf':conf})
+    result = [disp_dict[e] for e in out]
+    print(result)
+    out_str = ""
+    for i in result:
+        out_str += i[0]
+    with open(out_path, 'w+') as f:
+        f.write(out_str)
+        f.close()
